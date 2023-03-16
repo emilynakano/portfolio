fac37570eb119c06999d4743e4959f7a09d89ae2
emily <nakanodepaula@gmail.com>
2023-03-16 17:21:09 -0300

    feat: cube converted to tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
+import {Canvas} from '@react-three/fiber';
 import styled from 'styled-components';
 import InitialPage from './InitialPage';
+import Cube from '../components/Cube3D';
 export default function Home() {
 	return (
 		<Main>
 			<InitialPage />
+			<Canvas
+				camera={{
+					fov: 64,
+					position: [2.3, 1.5, 2.3],
+				}}
+			>
+				<Cube />
+			</Canvas>
+
 		</Main>
 	);
 }
