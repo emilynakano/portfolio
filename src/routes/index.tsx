067be4839deb2195a9faa8f8462f984e881e5f16
emily <nakanodepaula@gmail.com>
2023-03-16 08:13:37 -0300

    chore: three install

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from '../pages/Home';
 
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route />
+				<Route path='/' element={<Home />}/>
 			</Routes>
 		</BrowserRouter>
 	);
